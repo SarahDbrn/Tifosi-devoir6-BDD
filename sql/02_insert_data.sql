63c6e56a3ca16a59bf4c138bef44d18236ae8a14
SarahDebruyne <sarahdebruyne5@gmail.com>
2025-11-26 16:30:12 +0100

    ajout data tableaux

--- sql/02_insert_data.sql
+++ sql/02_insert_data.sql
@@ -1,10 +1,14 @@
 USE tifosi;
 
-SET SESSION local_infile = 1;
+-- On vide les tables dans le bon ordre
+DELETE FROM focaccia_ingredient;
+DELETE FROM boisson;
+DELETE FROM focaccia;
+DELETE FROM ingredient;
+DELETE FROM marque;
 
 -- 1) marque
-TRUNCATE TABLE marque;
-
+-- CSV : nom
 LOAD DATA LOCAL INFILE 'data/marque.csv'
 INTO TABLE marque
 FIELDS TERMINATED BY ',' 
@@ -14,8 +18,7 @@ IGNORE 1 LINES
 (nom);
 
 -- 2) ingredient
-TRUNCATE TABLE ingredient;
-
+-- CSV : nom
 LOAD DATA LOCAL INFILE 'data/ingredient.csv'
 INTO TABLE ingredient
 FIELDS TERMINATED BY ',' 
@@ -25,23 +28,23 @@ IGNORE 1 LINES
 (nom);
 
 -- 3) focaccia
-TRUNCATE TABLE focaccia;
-
+-- CSV : nom,prix,ingredients
+-- On ignore la colonne ingredients (texte)
 LOAD DATA LOCAL INFILE 'data/focaccia.csv'
 INTO TABLE focaccia
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"' 
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(nom, prix);
+(nom, prix, @ingredients_texte);
 
--- 4) boisson (avec marque jointe)
+-- 4) boisson
+-- CSV : nom,marque
 DROP TEMPORARY TABLE IF EXISTS boisson_tmp;
 
 CREATE TEMPORARY TABLE boisson_tmp (
   nom VARCHAR(150),
-  marque VARCHAR(100),
-  prix DECIMAL(6,2)
+  marque VARCHAR(100)
 );
 
 LOAD DATA LOCAL INFILE 'data/boisson.csv'
@@ -50,11 +53,11 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"' 
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(nom, marque, prix);
+(nom, marque);
 
-TRUNCATE TABLE boisson;
-
+-- On insère dans la vraie table boisson en reliant la marque texte à la table marque
 INSERT INTO boisson (nom, id_marque, prix)
-SELECT bt.nom, m.id_marque, bt.prix
+SELECT bt.nom, m.id_marque, 0
 FROM boisson_tmp bt
 JOIN marque m ON m.nom = bt.marque;
+
